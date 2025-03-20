68063dbd389d946f4b0e9c181a51f4504fd1a44a
qSugestio <danila2014204@gmail.com>
2025-03-20 13:04:35 +0300

    change styles

--- src/components/Other/Chart/CustomTooltip.tsx
+++ src/components/Other/Chart/CustomTooltip.tsx
@@ -6,7 +6,7 @@ const CustomTooltip = ({ active, payload, label }: any) => {
     return (
       <div className='bg-gray-800 p-3 rounded-lg shadow-lg border border-gray-700'>
         <p className='font-bold text-base'>{`День: ${label}`}</p>
-        <p className='text-base after:content-["▼"] text-(--text-focus)'>
+        <p className='text-base text-(--text-focus) after:content-["▼"] after:text-lg'>
           {`${formatNumber(data.price)}`}
         </p>
         {/* <p className='text-sm'>{`Цена: $${data.price.toFixed(2)}`}</p> */}
