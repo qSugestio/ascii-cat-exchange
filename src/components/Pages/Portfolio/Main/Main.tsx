9e0c210b8de34a0a4ecb983477a4ae63663ca949
qSugestio <danila2014204@gmail.com>
2025-03-20 13:23:37 +0300

    create Main Page

---
+++ src/components/Pages/Portfolio/Main/Main.tsx
@@ -0,0 +1,13 @@
+import BalanceInfo from './BalanceInfo'
+import Wallet from './Wallet'
+
+const Main = () => {
+  return (
+    <main className='size-full flex justify-center items-center'>
+      <BalanceInfo />
+      <Wallet />
+    </main>
+  )
+}
+
+export default Main
