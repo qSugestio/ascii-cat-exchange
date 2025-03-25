ff802a6088b0f29206da6dae81fbe2f8cef6b225
qSugestio <danila2014204@gmail.com>
2025-03-25 23:32:36 +0300

    center components laylout

--- client/src/components/Pages/Portfolio/Main/Main.tsx
+++ client/src/components/Pages/Portfolio/Main/Main.tsx
@@ -4,13 +4,16 @@ import Wallet from './Wallet'
 
 const Main = () => {
   return (
-    <main className='flex flex-col size-full justify-center items-center'>
-      <div>
-        <div className='flex flex-row'>
+    <main className='flex flex-col h-full w-full justify-center items-center'>
+      <div className='flex flex-row max-w-4xl w-full justify-center'>
+        <div className='flex-1 max-w-md'>
           <BalanceInfo />
+        </div>
+        <div className='flex-1 max-w-md'>
           <Wallet />
         </div>
-        <div></div>
+      </div>
+      <div className='max-w-4xl w-full'>
         <Assets />
       </div>
     </main>
