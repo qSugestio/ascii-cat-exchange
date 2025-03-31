9e41eb2e00374219851da244f92ed840c563cdb3
qSugestio <danila2014204@gmail.com>
2025-03-31 12:07:58 +0300

    add cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,6 +6,12 @@ const prismaService = new PrismaService();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:8080',
+    methods: 'GET,POST,PUT,DELETE',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap()
